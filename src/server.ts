aa836e790fe6bdf04ea4377dc559c2afa3813d75
bjekovicnik <n.bjekovic97@gmail.com>
2023-06-16 23:42:09 +0200

    server config

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+
+import { Logger } from "./utils/logger";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(process.env.PORT as String, () => {
+  Logger.info(`App is listening on port ${process.env.PORT}`);
+});
