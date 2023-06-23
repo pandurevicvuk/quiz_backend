ab305f3cb376ea3ccb7c9c983b02f2e09af463f7
bjekovicnik <n.bjekovic97@gmail.com>
2023-06-23 16:01:32 +0200

    update, delete user exception

--- src/service/user-service.ts
+++ src/service/user-service.ts
@@ -1,5 +1,6 @@
 import { PrismaClient, User } from "@prisma/client";
 import { UserSignUpDTO, UserUpdateDTO } from "../dto/user-dto";
+import { HttpException } from "../utils/http-exception";
 
 const prisma = new PrismaClient();
 
@@ -18,14 +19,22 @@ const getAll = async (id: number): Promise<User[]> => {
   return users;
 };
 
-const deleteUser = async (id: number): Promise<User> => {
-  var user = await prisma.user.delete({ where: { id: id } });
-  return user;
+const deleteUser = async (id: number): Promise<any> => {
+  try {
+    var user = await prisma.user.deleteMany({ where: { id: id } });
+    return user;
+  } catch (error) {
+    throw new HttpException(404, `User with id:$id not found!`);
+  }
 };
 
 const updateUser = async (id: number, dto: UserUpdateDTO): Promise<User> => {
-  var user = await prisma.user.update({ where: { id: id }, data: dto });
-  return user;
+  try {
+    var user = await prisma.user.update({ where: { id: id }, data: dto });
+    return user;
+  } catch (error) {
+    throw new HttpException(404, `User with id:$id not found!`);
+  }
 };
 
 export default {
