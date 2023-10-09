897774e60e9549d6470f472ad2a6c0fab7903b7b
bjekovicn <n.bjekovic97@gmail.com>
2023-10-09 15:49:05 +0200

    added questions service

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,1 +1,2 @@
 //export { default as  } from './';
+export {default as questionsServiceEN} from './question-service-en'
---
+++ src/service/question-service-en.ts
@@ -0,0 +1,128 @@
+import { db } from "../data/database";
+import { Op, Sequelize } from "sequelize";
+import { sequelize } from "../data/sequelize";
+
+const limit = 10;
+
+const getGameQuestions = async (
+  player1Id: number,
+  player2Id: number
+): Promise<number[]> => {
+  //
+  const [player1] = await db.QuestionsAnsweredEn.findOrCreate({
+    where: { userId: player1Id },
+    defaults: { userId: player1Id, questions: [] },
+  });
+  const [player2] = await db.QuestionsAnsweredEn.findOrCreate({
+    where: { userId: player2Id },
+    defaults: { userId: player2Id, questions: [] },
+  });
+
+  const combinedAnsweredQuestions = Array.from(
+    new Set([...player1.questions, ...player2.questions])
+  );
+
+  const unansweredQuestionsIdList = await db.QuestionsEn.findAll({
+    attributes: ["question_id"],
+    where: { id: { [Op.notIn]: combinedAnsweredQuestions } },
+    order: sequelize.random(),
+    limit: limit,
+  }).then((result) => result.map((e) => e.getDataValue("id")));
+
+  const additionalQuestionsCount = limit - unansweredQuestionsIdList.length;
+  if (additionalQuestionsCount === 0) return unansweredQuestionsIdList;
+
+  // Adding additional questions that were answered first
+  // Alternately - first half from player1, second half from player2
+
+  const additionalQuestionsIdList: number[] = [];
+  const list1: number[] = player1.questions;
+  const list2: number[] = player2.questions;
+
+  let i = 0;
+  while (i < Math.floor(additionalQuestionsCount / 2) && list1.length > 0) {
+    const questionId = list1.shift();
+    if (!additionalQuestionsIdList.includes(questionId!)) {
+      additionalQuestionsIdList.push(questionId!);
+      i++;
+    }
+  }
+
+  i = 0;
+  while (i < Math.ceil(additionalQuestionsCount / 2) && list2.length > 0) {
+    const questionId = list2.shift();
+    if (!additionalQuestionsIdList.includes(questionId!)) {
+      additionalQuestionsIdList.push(questionId!);
+      i++;
+    }
+  }
+
+  await db.QuestionsAnsweredEn.update(
+    { questions: Sequelize.literal(`ARRAY[${list1}]::INTEGER[]`) },
+    { where: { userId: player1Id } }
+  );
+  await db.QuestionsAnsweredEn.update(
+    { questions: Sequelize.literal(` ARRAY[${list2}]::INTEGER[]`) },
+    { where: { userId: player2Id } }
+  );
+
+  // Combine the unanswered and answered questions
+  return [...unansweredQuestionsIdList, ...additionalQuestionsIdList];
+};
+
+const updateAnswered = async (
+  userId: number,
+  answeredInGameList: number[]
+): Promise<void> => {
+  if (answeredInGameList.length === 0) return;
+
+  const answeredQuestionsRow = await db.QuestionsAnsweredEn.findOne({
+    where: { userId },
+  });
+
+  if (answeredQuestionsRow) {
+    const existingQuestionIds = answeredQuestionsRow.questions;
+    if (!existingQuestionIds) return;
+
+    const nerQuestionIdsSet = new Set([
+      ...existingQuestionIds,
+      ...answeredInGameList,
+    ]);
+    const newQuestionIds = Array.from(nerQuestionIdsSet);
+
+    const totalQuestions = await db.QuestionsEn.count();
+    if (newQuestionIds.length >= totalQuestions - 200) {
+      newQuestionIds.splice(0, newQuestionIds.length - (totalQuestions - 200));
+    }
+    answeredQuestionsRow.questions = newQuestionIds;
+    await answeredQuestionsRow.save();
+  } else {
+    await db.QuestionsAnsweredEn.create({
+      userId,
+      questions: answeredInGameList,
+    });
+  }
+};
+
+const getUnanswered = async (userId: number): Promise<number[]> => {
+  //
+  const [player] = await db.QuestionsAnsweredEn.findOrCreate({
+    where: { userId: userId },
+    defaults: { userId: userId, questions: [] },
+  });
+
+  const unansweredQuestionsIdList = await db.QuestionsEn.findAll({
+    attributes: ["question_id"],
+    where: { id: { [Op.notIn]: player.questions } },
+    order: sequelize.random(),
+    limit: limit,
+  }).then((result) => result.map((e) => e.getDataValue("id")));
+
+  return unansweredQuestionsIdList;
+};
+
+export default {
+  getGameQuestions,
+  updateAnswered,
+  getUnanswered,
+};
