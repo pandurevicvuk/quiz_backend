dc1272a613fabdd93033b3e68db4d14ce124d87d
bjekovicn <n.bjekovic97@gmail.com>
2024-02-12 12:19:04 +0100

    google register

--- src/service/user-service.ts
+++ src/service/user-service.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 import { db } from "../data/database";
 import { User } from "../data/models/users/users";
 import { HttpException } from "../utils/http-exception";
-import { Logger } from "../utils/logger";
 import { GoogleRegisterDTO } from "../dto/user-dto";
 
 const getById = async (userId: number): Promise<User> => {
@@ -13,57 +12,48 @@ const getById = async (userId: number): Promise<User> => {
   return user;
 };
 
-const createGuest = async (): Promise<any> => {
-  const maxRetries = 10;
-  var retries = 0;
+const createGuest = async (): Promise<User> => {
+  const guestType = await db.UserType.findOne({ where: { type: "GUEST" } });
 
-  while (retries < maxRetries) {
-    try {
-      const count = await db.User.count();
+  const user = await db.User.create({
+    active: true,
+    typeId: guestType!.id,
+    firstName: "guest",
+    lastName: "guest",
+  });
+  await db.UserStats.create({ id: user.id });
 
-      const { id, firstName, lastName, username, photo } = await db.User.create(
-        {
-          active: true,
-          firstName: `Guest`,
-          lastName: `Guest`,
-          username: `guest#${count + 1}`,
-          photo: "",
-          typeId: 1,
-        }
-      );
-      await db.UserStats.create({ id: id });
-
-      return { id, firstName, lastName, username, photo };
-    } catch (error) {
-      retries++;
-    }
-  }
-
-  throw new HttpException(400, "Failed to create guest user!");
+  return user;
 };
 
 const validateGoogleToken = async (dto: GoogleRegisterDTO): Promise<User> => {
   try {
     const response = await axios.get(
-      `https://oauth2.googleapis.com/tokeninfo?access_token=${dto.token}`
+      `https://oauth2.googleapis.com/tokeninfo?id_token=${dto.idToken}`
     );
+    const { given_name, family_name, email, sub, picture } = response.data;
 
-    const hasError = response.data.error_description;
-
-    if (hasError) throw new HttpException(401, "Google Validation failed!");
-
-    const user = await db.User.create({
-      active: true,
-      firstName: dto.firstName || "",
-      lastName: dto.lastName || "",
-      email: dto.email,
-      googleId: dto.googleId,
-      typeId: 1,
-      username: `user#${dto.googleId}`,
+    //ALREADY CREATED
+    const alreadyCreatedUser = await db.User.findOne({
+      where: { id: sub },
     });
+    if (alreadyCreatedUser) return alreadyCreatedUser;
+
+    //CREATE NEW
+    const playerType = await db.UserType.findOne({ where: { type: "PLAYER" } });
+    const user = await db.User.create({
+      typeId: playerType!.id,
+      active: true,
+      email: email,
+      googleId: sub,
+      photo: picture,
+      firstName: given_name,
+      lastName: family_name,
+    });
+
     return user;
   } catch (err: any) {
-    throw new HttpException(400, err.response?.data);
+    throw new HttpException(400, err.response.data);
   }
 };
 
